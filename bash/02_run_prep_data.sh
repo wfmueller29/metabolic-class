7c97c3825067c485ee47a2d411ee512141b937da
Billy Mueller <wfmueller29@gmail.com>
2023-02-03 14:40:49 -0500

    Fixed naming and added module load option to 02_run_prep_data.sh

--- bash/02_run_prep_data.sh
+++ bash/02_run_prep_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-module load R/4.1
+module load R/4.1 || echo WARNING: Could not load R module, continuing anyway
 
 cd 02_prep_model_data/
 
-Rscript R/prep_model_data.R
+Rscript R/general_prep_model_data.R
