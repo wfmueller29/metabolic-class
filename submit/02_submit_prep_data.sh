e09a861c473e2b216d55ffd754b249ee6695fb6e
Billy Mueller <wfmueller29@gmail.com>
2023-02-07 16:42:30 -0500

    Submit 02_prep_model_data now takes command line input

--- submit/02_submit_prep_data.sh
+++ submit/02_submit_prep_data.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+CONFIG=$1
+
 sbatch --mem=20g \
   --cpus-per-task=2 \
   -t 1-00:00:00 \
   -o log/prep_data-slurmo-%A_%a.out \
   -e log/prep_data-slurme-%A_%a.out \
+  --export=CONFIG=$CONFIG \
   --mail-type=END bash/02_run_prep_data.sh
