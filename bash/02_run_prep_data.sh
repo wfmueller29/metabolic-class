58e8c85c7c64fc8d794a3ae4df7691a522eb59c1
Billy Mueller <wfmueller29@gmail.com>
2023-02-07 16:41:53 -0500

    Can run bash 02 and 03 without config but will give warning

--- bash/02_run_prep_data.sh
+++ bash/02_run_prep_data.sh
@@ -4,4 +4,21 @@ module load R/4.1 || echo WARNING: Could not load R module, continuing anyway
 
 cd 02_prep_model_data/
 
-Rscript R/general_prep_model_data.R
+if [ -z "$1" ]
+    then
+        echo No command line argument provided
+    else 
+        CONFIG=$1
+        CONFIG=$(echo $CONFIG | cut -d'/' -f2-)
+fi
+
+echo $CONFIG
+
+if [ -z $CONFIG ]
+    then 
+        echo WARNING: No config file provided
+        Rscript R/general_prep_model_data.R
+    else 
+        echo Using config: $CONFIG
+        Rscript R/general_prep_model_data.R $CONFIG 
+fi
--- bash/03_run_models.sh
+++ bash/03_run_models.sh
@@ -7,5 +7,21 @@ module load R/4.1 || echo WARNING: Could not load R module, continuing anyway
 # change dir
 cd 03_model/
 
-# Run slam2_models
-Rscript R/model.R $CONFIG
+if [ -z "$1" ]
+    then
+        echo No command line argument provided
+    else 
+        CONFIG=$1
+        CONFIG=$(echo $CONFIG | cut -d'/' -f2-)
+fi
+
+echo $CONFIG
+
+if [ -z $CONFIG ]
+    then 
+        echo WARNING: No config file provided
+        Rscript R/model.R
+    else 
+        echo Using config: $CONFIG
+        Rscript R/model.R $CONFIG 
+fi
