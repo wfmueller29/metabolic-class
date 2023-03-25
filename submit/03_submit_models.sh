70880deb87fff0a57feb133261067169f5508f84
Billy Mueller <wfmueller29@gmail.com>
2023-03-25 14:05:14 -0400

    Fixed slam_march2023 prediction_data duplication
    Also changed CONFIG variable to OUT_TAG in 03_model

--- submit/03_submit_models.sh
+++ submit/03_submit_models.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-CONFIG=$1
+OUT_TAG=$1
 
 sbatch --mem=100g \
   --cpus-per-task=50 \
   -t 4-00:00:00 \
   -o log/models-slurmo-%A_%a.out \
   -e log/models-slurme-%A_%a.out \
-  --export=CONFIG=$CONFIG \
+  --export=OUT_TAG=$OUT_TAG \
   --mail-type=END bash/03_run_models.sh
