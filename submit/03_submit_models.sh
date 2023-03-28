1e49f5bb0306af6213ef778048b03f0de12ee49b
Billy Mueller <wfmueller29@gmail.com>
2023-03-28 17:34:57 -0400

    Increased 03_model walltime to 10 days

--- submit/03_submit_models.sh
+++ submit/03_submit_models.sh
@@ -4,7 +4,7 @@ OUT_TAG=$1
 
 sbatch --mem=100g \
   --cpus-per-task=50 \
-  -t 4-00:00:00 \
+  -t 10-00:00:00 \
   -o log/models-slurmo-%A_%a.out \
   -e log/models-slurme-%A_%a.out \
   --export=OUT_TAG=$OUT_TAG \
