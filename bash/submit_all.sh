0b6669185da65f512532b037b1ce7e9fe2688624
Billy Mueller <wfmueller29@gmail.com>
2023-04-11 10:06:43 -0400

    Created bash/submit_all.sh to run all the jobs

---
+++ bash/submit_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+bash submit/02_03_04_04c_submit.sh 02_prep_model_data/yaml/slam_age_all.yaml
+bash submit/02_03_04_04c_submit.sh 02_prep_model_data/yaml/slam_age_subset.yaml
+bash submit/02_03_04_04c_submit.sh 02_prep_model_data/yaml/slam_perle_all.yaml
+bash submit/02_03_04_04c_submit.sh 02_prep_model_data/yaml/slam_perle_subset.yaml
