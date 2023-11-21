7035ce7a464946c6d28d8068ff4a8aedd3fe7b1b
William Mueller <williammueller@williams-mbp.dhcp.hofstra.edu>
2023-11-21 17:59:36 -0500

    Reformatted

--- 05_figures/render_figures.R
+++ 05_figures/render_figures.R
@@ -2,17 +2,21 @@
 
 render_model_output <- function(model_dir) {
   output_dir <- normalizePath(file.path("output", model_dir))
-  rmarkdown::render(input = "05_figures.Rmd",
-                  output_dir = output_dir,
-                  params = list(model_dir = model_dir))
+  rmarkdown::render(
+    input = "05_figures.Rmd",
+    output_dir = output_dir,
+    params = list(model_dir = model_dir)
+  )
 }
 
 
 # get all directories within the 03_model/output directory
 
-dirs <- list.dirs(path = "../04_model_select/output/.",
-                  full.names = FALSE,
-                  recursive = FALSE)
+dirs <- list.dirs(
+  path = "../04_model_select/output/.",
+  full.names = FALSE,
+  recursive = FALSE
+)
 
 # drop archive dir
 dirs <- dirs[dirs != "archive"]
