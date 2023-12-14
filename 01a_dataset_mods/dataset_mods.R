106911353f89c9c9b0e7f28f606cce95bd2519b4
William Mueller <williammueller@williams-mcp.dhcp.hofstra.edu>
2023-12-14 18:28:06 -0500

    Creating new file for dataset modifications

---
+++ 01a_dataset_mods/dataset_mods.R
@@ -0,0 +1,3 @@
+# This is where we are going to put all of the data subsetting, such as
+# removing outliers, and subsetting by sex and strain. The logic here is to
+# greatly simplify the modelling pipeline
