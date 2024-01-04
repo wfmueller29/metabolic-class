40260e0ed658bdcfd9719e8b38f2b4c045c635af
William Mueller <williammueller@williams-mcp.dhcp.hofstra.edu>
2024-01-03 19:49:29 -0500

    Removed dplyr and added generalized id_var for plot_obs

--- 05_figures/R/plot_obs.R
+++ 05_figures/R/plot_obs.R
@@ -9,6 +9,7 @@ traj_obs <- function(main,
                      t1,
                      age_var,
                      y_var,
+                     id_var,
                      title = " ",
                      xlab = " ",
                      ylab = " ") {
@@ -18,42 +19,39 @@ traj_obs <- function(main,
   # age_var_ns
   age_var <- paste0(age_var, "_ns")
 
-  ## make id character
-  df <- df %>%
-    select(-sex, -strain)
-
   ## join main and df
-  main_obs <- main %>%
-    left_join(df, by = "idno")
+  main_obs <- merge(x = main, y = df, by = id_var)
 
-  p <- ggplot(
+  p <- ggplot2::ggplot(
     data = main_obs,
-    mapping = aes(
+    mapping = ggplot2::aes(
       x = eval(as.symbol((age_var))),
       y = eval(as.symbol(y_var)),
       color = factor(new_class)
     )
   ) +
-    geom_point(alpha = .1) +
-    geom_smooth(method = "gam",
-                formula = y ~ s(x, bs = "cs"),
-                span = .7,
-                inherit.aes = TRUE) +
-    scale_color_manual(values = lej$col) +
-    labs(
+    ggplot2::geom_point(alpha = .1) +
+    ggplot2::geom_smooth(
+      method = "gam",
+      formula = y ~ s(x, bs = "cs"),
+      span = .7,
+      inherit.aes = TRUE
+    ) +
+    ggplot2::scale_color_manual(values = lej$col) +
+    ggplot2::labs(
       color = "Class",
       y = ylab,
       x = xlab,
       title = title
     ) +
-    theme(
-      plot.title = element_text(
+    ggplot2::theme(
+      plot.title = ggplot2::element_text(
         face = "bold",
         color = "black",
         size = 16,
         hjust = .5
       ),
-      plot.margin = unit(c(10, 4, 25, 2), "pt")
+      plot.margin = ggplot2::unit(c(10, 4, 25, 2), "pt")
     )
 
   return(p)
