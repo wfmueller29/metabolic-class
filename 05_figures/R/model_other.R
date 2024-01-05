b1b1891dc33d56791fb1ed88afab7f93c1610635
William Mueller <williammueller@williams-mcp.dhcp.hofstra.edu>
2024-01-04 19:05:55 -0500

    Refactored model other

--- 05_figures/R/model_other.R
+++ 05_figures/R/model_other.R
@@ -3,26 +3,27 @@
 # Author: William Mueller
 #
 
-model_other <- function(census,
-                        other_df,
-                        fixcovs,
-                        outcome,
-                        age_var,
-                        class,
-                        subject) {
-  new_df <- create_other_plot_df(census, other_df)
-  
-  if (length(new_df$idno) == 0 ) {
+model_combined <- function(census,
+                           outcome_df,
+                           fixcovs,
+                           outcome,
+                           age_vars,
+                           class,
+                           census_id,
+                           outcome_id) {
+  combined_df <- create_combined_df(census, census_id, outcome_df, outcome_id)
+
+  if (nrow(combined_df) == 0) {
     return("No overlapping subjects")
   }
-  
-  if (length(unique(new_df$idno)) < 2) {
+
+  if (length(unique(combined_df[[outcome_id]])) < 2) {
     return("Need more than one subject to model")
   }
 
   # Keep fixcov if there are more than one value for the factor
   keep_fixcovs <- lapply(fixcovs, function(fixcov) {
-    l <- length(unique(new_df[[fixcov]]))
+    l <- length(unique(combined_df[[fixcov]]))
     if (l == 1) {
       keep <- FALSE
     } else {
@@ -33,20 +34,19 @@ model_other <- function(census,
 
   fixcovs <- fixcovs[as.logical(keep_fixcovs)]
 
-  age_var2 <- paste0(age_var, "2")
-  age_var <- paste(age_var, "ns", sep = "_")
-  age_var2 <- paste(age_var2, "ns", sep = "_")
-  if (length(unique(new_df[[class]])) == 1) {
-    class_term0 <- paste(age_var, age_var2, sep = " + ")
-    class_term1 <- paste(age_var, age_var2, sep = " + ")
-  } else {
-    class_term0 <- paste(age_var, age_var2, class, sep = " + ")
-    class_term1 <- paste("(", age_var, "+", age_var2, ")", "*", class)
+  age_vars <- paste0(age_vars, "_ns")
+
+  age_sum <- paste(age_vars, collapse = " + ")
+  class_term0 <- age_sum
+  class_term1 <- age_sum
+  if (length(unique(combined_df[[class]])) != 1) {
+    class_term0 <- paste(age_sum, class, sep = " + ")
+    class_term1 <- paste0("(", age_sum, ")", " * ", class)
   }
-  raneff <- paste("(", age_var, "+", age_var2, "|", subject, ")", sep = " ")
+  raneff <- paste0("(", age_sum, " | ", census_id, ")")
   fixeff0 <- paste(outcome, "~", class_term0)
   fixeff1 <- paste(outcome, "~", class_term1)
-  interact <- paste("+", "(", age_var, "+", age_var2, ")", "*", sep = " ")
+  interact <- paste0("+ ", "(", age_sum, ")", " * ")
   if (length(fixcovs) > 0) {
     fixeff0.5 <- paste(fixeff0, paste(fixcovs, collapse = " * "), sep = " + ")
     fixeff2 <- paste(fixeff1, paste(fixcovs, collapse = " * "), sep = " + ")
@@ -64,29 +64,32 @@ model_other <- function(census,
   fixeff_list <- list(fixeff0, fixeff0.5, fixeff1, fixeff2, fixeff3)
   formula_list <- lapply(fixeff_list, paste, "+", raneff)
   formula_list <- lapply(formula_list, as.formula)
-  model_list <- lapply(formula_list, lmer, data = new_df)
+  model_list <- lapply(formula_list, lme4::lmer, data = combined_df)
 
   model_list
 }
 
 
-model_other_apply <- function(final_models, model_name, fixcovs, class) {
-  x <- final_models
-  other_df <- x[x$model_name == model_name, ]$dfs[[1]]
-  outcome <- x[x$model_name == model_name, ]$oc
-  models <- lapply(seq_len(nrow(x)), function(i) {
-    model_other(
-      census = x$census[[i]],
-      other_df = other_df,
+model_other_apply <- function(final_models, datasets,
+                              model_name, fixcovs, class) {
+  final_models_row <- final_models[final_models$model_name == model_name, ]
+  outcome_df <- final_models_row$dfs[[1]]
+  outcome_id <- final_models_row$subject
+  outcome <- final_models_row$oc
+  dataset_index <- final_models_row$dataset_index
+  age_vars <- datasets[[dataset_index]]$age_var
+  fixcovs <- final_models_row$fixcov[[1]]
+  models <- lapply(seq_len(nrow(final_models)), function(i) {
+    model_combined(
+      census = final_models$census[[i]],
+      outcome_df = outcome_df,
       fixcovs = fixcovs,
       outcome = outcome,
-      age_var = x$age_var[[i]],
+      age_vars = age_vars,
       class = class,
-      subject = x$subject[[i]]
+      census_id = final_models$subject[[i]],
+      outcome_id = outcome_id
     )
-    print(i)
   })
   models
-  
 }
-
