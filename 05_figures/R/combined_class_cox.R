e76ff1ac60486f5e62be6e31343dc635be396162
William Mueller <williammueller@williams-mbp.mynetworksettings.com>
2024-01-09 20:02:38 -0500

    Generalized combined cox for different id's; factored out dplyr

--- 05_figures/R/combined_class_cox.R
+++ 05_figures/R/combined_class_cox.R
@@ -6,58 +6,43 @@
 # list in the yaml configuration file.
 combine_census <- function(model_name_vector,
                            final_model_object,
-                           datasets_object,
                            dead_censor,
                            age_death,
                            covariates) {
-
-  # things we can parameterize, but not doing it today
-  fixcov <- covariates
-
-
   # rename inputs
   final_models <- final_model_object
-  datasets <- datasets_object
 
   # get list of censuses
   model_index <- final_models$model_name %in% model_name_vector
   censuses <- final_models[model_index, ]$census
-
-  # get datasets for models specified by model_name_vect so we can get their
-  # id's
-  dataset_names <- stringr::str_split(model_name_vector, "\\|", simplify = TRUE)[, 1]
-  dataset_index <- names(datasets) %in% dataset_names
-  datasets <- datasets[dataset_index]
-
-  # get ids of datasets
-  ids <- lapply(datasets, function(dataset) dataset$id)
-  ids_unique <- unique(unlist(ids))
-
-  if (length(ids_unique) != 1) stop("ID's for each census do not match")
+  ids <- final_models[model_index, "subject"]
 
   # Select probability of class membership and id's
-  censuses_bare <- lapply(censuses, function(census) {
-
+  censuses_bare <- mapply(function(census, id) {
     # get prob column names
     probs <- names(census)[grepl("^prob", names(census))]
-    census <- census[, c(ids_unique, probs)]
-  })
+    census <- census[, c(id, probs)]
+  }, census = censuses, id = ids)
 
-  # merge census into one dataframe
-  merged_census <- censuses_bare %>%
-    purrr::reduce(dplyr::left_join, by = ids_unique)
+  xy_list <- mapply(function(census, id) {
+    list(census = census, id = id)
+  }, census = censuses_bare, id = ids, SIMPLIFY = FALSE)
 
-  # filter out NA's in merge census
+  merged_census <- Reduce(function(x, y) {
+    census <- merge(x = x$census, y = y$census, by.x = x$id, by.y = y$id)
+    id <- x$id
+    list(census = census, id = id)
+  }, xy_list)
+
+  merged_census_id <- merged_census$id
+  merged_census <- merged_census$census
   merged_census <- merged_census[complete.cases(merged_census), ]
 
-  # add merged census back to original census
-  censuses <- lapply(censuses, function(census) {
-    census <- census %>%
-      dplyr::select(-rsample::starts_with("prob"))
-
-    census <- merged_census %>%
-      dplyr::left_join(census, by = ids_unique)
-  })
+  censuses <- mapply(function(census, id) {
+    census <- census[, !grepl("^prob", colnames(census))]
+    census <- merge(census, merged_census, by.x = id, by.y = merged_census_id)
+    census
+  }, census = censuses, id = ids, SIMPLIFY = FALSE)
 
   # remove outcome specific names from the census
   censuses <- lapply(censuses, function(census) {
@@ -65,7 +50,10 @@ combine_census <- function(model_name_vector,
   })
 
   # check if all censuses are equal (because they should be)
-  check <- do.call(all.equal, censuses)
+  check <- lapply(censuses, function(census) {
+    all.equal(census, censuses[[1]])
+  })
+  check <- do.call(all.equal, check)
   if (!isTRUE(check)) {
     print(check)
     stop("Not all censuses are equal \n")
@@ -100,14 +88,12 @@ cox_merged_class <- function(merged_census,
 
 cox_combine_class <- function(model_name_vector,
                               final_model_object,
-                              datasets_object,
                               covariates,
                               age_death,
                               censor) {
   merged_census <- combine_census(
     model_name_vector,
     final_model_object,
-    datasets_object,
     covariates,
     age_death,
     censor
