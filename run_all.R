ceeda31293d7866579d9854f935110921a3cef10
William Mueller <williammueller@williams-mcp.dhcp.hofstra.edu>
2024-02-08 19:23:16 -0500

    Fixed path bug in run all

--- run_all.R
+++ run_all.R
@@ -3,12 +3,13 @@
 args <- commandArgs(trailingOnly = TRUE)
 
 if (length(args) == 0) {
-  args[[1]] <- "input/test_local.yaml"
+  args[[1]] <- "01_prep_model_data/input/test_local.yaml"
   warning("No input file provided, using: ", args[[1]])
 }
 
-setwd("01_prep_model_data")
 args[[1]] <- normalizePath(args[[1]])
+
+setwd("01_prep_model_data")
 config <- yaml::read_yaml(args[[1]])
 system2("Rscript", args = c("01_prep_model_data.R", args[[1]]))
 setwd("..")
