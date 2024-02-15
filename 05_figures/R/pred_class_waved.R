2338c7f4ecc45cd233e986bc97c9186fd13d932a
William Mueller <williammueller@williams-mcp.dhcp.hofstra.edu>
2024-02-15 17:31:33 -0500

    Included apply wrappers for predicting class for subsetted test_data

--- 05_figures/R/pred_class_waved.R
+++ 05_figures/R/pred_class_waved.R
@@ -31,3 +31,57 @@ predict_waved_class <- function(newdata,
 
   waved_predictions
 }
+
+# Function that will predict given a single model, but a list of prediction
+# data.frames
+predict_waved_class_newdata_list <- function(newdata_list, model, age, id) {
+  if (!is.list(newdata_list)) {
+    warning("newdata_list is not a list; returning NA")
+    return(NA)
+  }
+  subset_predictions <- lapply(newdata_list, function(newdata) {
+    # check if there is actually data in the newdata
+    if (nrow(newdata) >= 2) {
+      class_result <- predict_waved_class(
+        newdata = newdata,
+        model = model,
+        age = age,
+        id = id
+      )
+    } else {
+      col_names <- names(model$pprob)
+      class_result <- data.frame(matrix(nrow = 0, ncol = length(col_names)))
+      names(class_result) <- col_names
+    }
+
+    class_result
+  })
+
+  # check if NA in subset_predictions and remove that data
+  keep <- !is.na(subset_predictions)
+  subset_predictions <- subset_predictions[keep]
+
+  names(subset_predictions) <- names(newdata_list)[keep]
+
+  subset_predictions
+}
+
+predict_waved_class_model_list <- function(nested_newdata_list,
+                                           model_list,
+                                           names,
+                                           age_list,
+                                           id_list) {
+  pred_result <- lapply(seq_along(model_list), function(i) {
+    print(i)
+    predict_waved_class_newdata_list(
+      newdata_list = nested_newdata_list[[i]],
+      model = model_list[[i]],
+      age = age_list[[i]],
+      id = id_list[[i]]
+    )
+  })
+
+  names(pred_result) <- names
+
+  pred_result
+}
