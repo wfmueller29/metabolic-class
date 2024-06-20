2df9f6d7d3a7bcb9de4fc323018d1fb3ab90916d
William Mueller <williammueller@williams-mcp.local>
2024-06-20 00:23:35 -0400

    Included input yaml from cmd line for run_4-5.R

--- run_4-5.R
+++ run_4-5.R
@@ -1,6 +1,15 @@
 # this is for running 4-5
 
-config$out_tag <- "all_realtive_age" # delete this
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) == 0) {
+  args[[1]] <- "01_prep_model_data/input/test_local.yaml"
+  warning("No input file provided, using: ", args[[1]])
+}
+
+args[[1]] <- normalizePath(args[[1]])
+cat("This is our input file:", args[[1]], "\n")
+config <- yaml::read_yaml(args[[1]])
 
 # 03 -------------------------------------------------------------------------
 
