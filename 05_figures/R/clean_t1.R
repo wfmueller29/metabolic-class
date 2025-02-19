91fbd3a3fc7c4e1cd40bdf4813f2ff9e8e453480
William Mueller <williammueller@williams-mcp.mynetworksettings.com>
2025-02-19 00:45:35 -0500

    Created ci for mle in clean_t1 and rounded pvals from chi-test

--- 05_figures/R/clean_t1.R
+++ 05_figures/R/clean_t1.R
@@ -32,11 +32,16 @@ clean_t1 <- function(census,
     stop("T1 is not NA or a data.frame")
   }
 
-  mle_ci <- create_mle_with_ci(
+  mle_ci <- create_mle(
     data = census,
     age_death = age_death,
-    event = event
+    event = event,
+    ci = TRUE
   )
+  mle_ci$total <- NA
+  mle_ci$pval <- NA
+  clean_t1 <- clean_t1[-nrow(clean_t1), ]
+  clean_t1 <- rbind(clean_t1, mle_ci)
 
   clean_t1
 }
@@ -71,7 +76,7 @@ create_clean_t1 <- function(df_table,
 
   for (col in columns) {
     if (class(chi_tests[[col]]) == "htest") {
-      pvals[[col]] <- chi_tests[[col]]$p.value
+      pvals[[col]] <- signif(chi_tests[[col]]$p.value, 4)
     } else if (is.na(chi_tests[[col]])) {
       pvals[[col]] <- NA
     } else {
@@ -89,111 +94,31 @@ create_clean_t1 <- function(df_table,
   t1
 }
 
-create_mle_with_ci <- function(data,
-                               age_death,
-                               event) {
-  browser()
-  df_main <- df
-
-  df_total <- aggregate(df_main[, "new_class"],
-    by = list(new_class = df_main[["new_class"]]),
-    FUN = length
-  )
-  names(df_total)[names(df_total) == "x"] <- "n"
-
-  df_list_count_by_class <- lapply(
-    columns,
-    count_column_by_class,
-    df = df, group = "new_class"
-  )
+create_mle <- function(data,
+                       age_death,
+                       event,
+                       ci = FALSE) {
+  df_main <- data
 
   obj <- survival::Surv(time = df_main[[age_death]], event = df_main[[event]])
   surv_fit <- survival::survfit(data = df_main, obj ~ factor(new_class))
 
   df_surv <- survminer::surv_median(surv_fit)
-  df_surv$median <- paste0(
-    df_surv$median,
-    " (", df_surv$lower, ", ", df_surv$upper, ")"
-  )
+  if (ci) {
+    df_surv$median <- paste0(
+      df_surv$median,
+      " (", df_surv$lower, ", ", df_surv$upper, ")"
+    )
+  }
   names(df_surv)[names(df_surv) == "strata"] <- "new_class"
   names(df_surv)[names(df_surv) == "median"] <- "median_surv"
   df_surv <- df_surv[, !(colnames(df_surv) == c("lower", "upper"))]
 
   df_surv$new_class <- sort(unique(df_main$new_class))
 
-  dfs <- df_list_count_by_class
+  rownames(df_surv) <- paste0("class_", df_surv[, 1], "_final")
+  df_surv <- df_surv[, -1, drop = FALSE]
+  df_surv <- as.data.frame(t(df_surv))
 
-  dfs <- c(list(df_total), dfs)
-
-  dfs <- c(dfs, list(df_surv))
-
-  df_table <- Reduce(function(x, y) merge(x, y, by = "new_class"), dfs)
-  df_table <- data.frame(t(df_table))
-  names(df_table) <- as.character(unlist(df_table[1, ]))
-
-  # for edge case when there is one class, we need to store names and reassign
-  df_table <- df_table[-1, , drop = FALSE]
-
-  df_table[is.na(df_table)] <- 0
-  names(df_table) <- paste0("class_", names(df_table))
-  seq_cols <- seq_along(df_table)
-  df_table[, seq_cols] <- lapply(df_table[, seq_cols], as.numeric)
-  df_table <- round(df_table)
-
-  df_table
-}
-
-
-create_mle_with_ci <- function(data,
-                               age_death,
-                               event) {
-  browser()
-  df_main <- df
-
-  df_total <- aggregate(df_main[, "new_class"],
-    by = list(new_class = df_main[["new_class"]]),
-    FUN = length
-  )
-  names(df_total)[names(df_total) == "x"] <- "n"
-
-  df_list_count_by_class <- lapply(
-    columns,
-    count_column_by_class,
-    df = df, group = "new_class"
-  )
-
-  obj <- survival::Surv(time = df_main[[age_death]], event = df_main[[event]])
-  surv_fit <- survival::survfit(data = df_main, obj ~ factor(new_class))
-
-  df_surv <- survminer::surv_median(surv_fit)
-  df_surv$median <- paste0(
-    df_surv$median,
-    " (", df_surv$lower, ", ", df_surv$upper, ")"
-  )
-  names(df_surv)[names(df_surv) == "strata"] <- "new_class"
-  names(df_surv)[names(df_surv) == "median"] <- "median_surv"
-  df_surv <- df_surv[, !(colnames(df_surv) == c("lower", "upper"))]
-
-  df_surv$new_class <- sort(unique(df_main$new_class))
-
-  dfs <- df_list_count_by_class
-
-  dfs <- c(list(df_total), dfs)
-
-  dfs <- c(dfs, list(df_surv))
-
-  df_table <- Reduce(function(x, y) merge(x, y, by = "new_class"), dfs)
-  df_table <- data.frame(t(df_table))
-  names(df_table) <- as.character(unlist(df_table[1, ]))
-
-  # for edge case when there is one class, we need to store names and reassign
-  df_table <- df_table[-1, , drop = FALSE]
-
-  df_table[is.na(df_table)] <- 0
-  names(df_table) <- paste0("class_", names(df_table))
-  seq_cols <- seq_along(df_table)
-  df_table[, seq_cols] <- lapply(df_table[, seq_cols], as.numeric)
-  df_table <- round(df_table)
-
-  df_table
+  df_surv
 }
