d487e4d4e463e23916fda187f5151efcc6f78b59
William Mueller <williammueller@williams-mcp.local>
2025-02-20 22:43:44 -0500

    Parameterized 00c_survival_data

--- 00c_survival_data/create_survival_data.R
+++ 00c_survival_data/create_survival_data.R
@@ -1,7 +1,17 @@
 # Just to write survival data as CSV
 # Author: William Mueller
 
-load("../00a_dataset/output/main_cat_surv.RDATA")
+# load in config using file path ----------------------------------------------
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) == 0) {
+  args[[1]] <- "input/slam_c16-c18.yaml"
+}
+
+config <- yaml::read_yaml(args[[1]])
+
+
+load(config$path)
 
 keep_cols <- c(
   "idno", "dead_censor", "fu_age_wk", "le_wk", "cod",
@@ -12,5 +22,27 @@ main_cat_surv <- main_cat_surv[keep_cols]
 
 main_cat_surv$percent_le <- 100
 
-dir.create("output")
-write.csv(x = main_cat_surv, file = "output/main_cat_surv.csv")
+# save all datsets as csv files in output/data --------------------------------
+output_name <- basename(args[[1]])
+output_name <- strsplit(x = output_name, split = "\\.")[[1]][[1]]
+file_path <- file.path("output", output_name, "data")
+if (dir.exists(file_path)) {
+  files <- list.files(file_path)
+  files <- file.path(file_path, files)
+  file.remove(files)
+} else {
+  dir.create(file_path, recursive = TRUE)
+}
+
+file_name <- file.path(file_path, "main_cat_surv.csv")
+write.csv(x = main_cat_surv, file = file_name)
+
+
+# create output file ----------------------------------------------------------
+file_name <- "main_cat_surv.csv"
+names(file_name) <- file.path(getwd(), "output", "data", output_name)
+
+output_name <- basename(args[[1]])
+output_path <- file.path("output", output_name)
+yaml::write_yaml(x = file_name, file = output_path)
+
