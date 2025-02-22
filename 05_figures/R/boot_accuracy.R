da45ad95b9945707f2604f1514a6cf6b3d761df0
William Mueller <williammueller@williams-mcp.local>
2025-02-21 20:29:40 -0500

    Including parallel computing for bootstrapping accuracy

--- 05_figures/R/boot_accuracy.R
+++ 05_figures/R/boot_accuracy.R
@@ -4,7 +4,11 @@
 # for make_new_df function we must source
 source("R/pred_table.R")
 
-boot_accuracy <- function(pred_df, og_df, subject) {
+boot_accuracy <- function(pred_df,
+                          og_df,
+                          subject,
+                          parallel = "no",
+                          ncpus = getOption("boot.ncpus", 1L)) {
   if (nrow(pred_df) == 0) {
     warning("pred_df has 0 rows, cannot make_new_df, returning NA")
     return(NA)
@@ -53,12 +57,16 @@ boot_accuracy <- function(pred_df, og_df, subject) {
     # f1 <- MLmetrics::F1_Score(y_true = data$class.og, y_pred = data$class.pred)
     # f1 <- f1_score(predicted = data$class.pred, expected = data$class.og)
     f1
-  }, R = 2000)
+  }, R = 2000, parallel = parallel, ncpus = ncpus)
 
   bootobj
 }
 
 
-boot_accuracy_list <- function(pred_df_list, og_df, subject) {
-  lapply(pred_df_list, boot_accuracy, og_df, subject)
+boot_accuracy_list <- function(pred_df_list, og_df, subject, parallel = "no",
+                               ncpus = getOption("boot.ncpus", 1L)) {
+  lapply(pred_df_list, boot_accuracy,
+    og_df, subject,
+    parallel = parallel, ncpus = ncpus
+  )
 }
