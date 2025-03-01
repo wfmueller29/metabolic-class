0ce4b9dc27ac8dff6c5613add3e1f352ed3ad6ee
William Mueller <williammueller@williams-mcp.local>
2025-02-28 21:20:32 -0500

    Restructured to accomdate individual_cox formulas

--- 05_figures/R/model_cox.R
+++ 05_figures/R/model_cox.R
@@ -2,39 +2,13 @@
 # controlling for sex and strain
 # Author: William Mueller
 
-model_cox <- function(census, var, covariates, age_death, censor) {
+model_cox <- function(census, formulas, age_death, censor) {
   # Trying to deal with the case when length class == 1 (not sure how this will
   # work)
-  len_var <- length(unique(census[[var]]))
-  if (len_var == 1) {
-    census[[var]] <- as.numeric(census[[var]])
-  }
 
-  keep_covariates <- lapply(covariates, function(cov) {
-    l <- length(unique(census[[cov]]))
-    if (l == 1) {
-      FALSE
-    } else {
-      TRUE
-    }
-  })
-
-  covariates <- covariates[as.logical(keep_covariates)]
-
-  if (length(covariates) == 0) {
-    rhs1 <- var
-    rhs2 <- var
-    rhs3 <- var
-  } else {
-    rhs1 <- var
-    rhs2 <- paste(var, "+", paste(covariates, collapse = " + "))
-    rhs3 <- paste(var, "+", paste(covariates, collapse = " * "))
-  }
-
-
-  f_list <- list(rhs1, rhs2, rhs3)
+  f_list <- formulas
   model_list <- lapply(f_list, function(form) {
-    form <- paste("surv_object", form, sep = " ~ ")
+    form <- paste("surv_object", form)
     form <- as.formula(form)
     surv_object <- survival::Surv(time = census[[age_death]],
                                   event = census[[censor]])
