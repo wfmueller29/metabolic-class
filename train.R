e0db3bca1a175df1bfaf1f2a741e763d7eff3a69
William Mueller <williammueller@williams-mcp.mynetworksettings.com>
2025-04-13 21:04:47 -0400

    Updating files with new naming convention

--- train.R
+++ train.R
@@ -46,32 +46,42 @@ input_04 <- normalizePath(
   paste0(file.path("03_model_select/output", config$out_tag), ".yaml")
 )
 
-setwd("05_prediction_data")
-system2("Rscript", args = c("create_prediction_data.R", input_04))
+setwd("04_create_census")
+system2("Rscript", args = c("create_census.R", input_04))
 setwd("..")
 
 # 05 -------------------------------------------------------------------------
 
 input_05 <- normalizePath(
-  paste0(file.path("05_prediction_data/output", config$out_tag), ".yaml")
+  paste0(file.path("04_create_census/output", config$out_tag), ".yaml")
+)
+
+setwd("05_prediction_data")
+system2("Rscript", args = c("create_prediction_data.R", input_05))
+setwd("..")
+
+# 06 -------------------------------------------------------------------------
+
+input_06 <- normalizePath(
+  paste0(file.path("06_prediction_data/output", config$out_tag), ".yaml")
 )
 
 setwd("06_create_figures")
 output_dir <- normalizePath(file.path("output", config$out_tag))
 rmarkdown::render("06_create_figures.Rmd",
-  output_dir = output_dir, params = list(input_path = input_05)
+  output_dir = output_dir, params = list(input_path = input_06)
 )
 setwd("..")
 
-# 06 --------------------------------------------------------------------------
-input_06 <- normalizePath(
-  paste0(file.path("06_create_figures/output", config$out_tag), ".yaml")
+# 07 --------------------------------------------------------------------------
+input_07 <- normalizePath(
+  paste0(file.path("07_create_figures/output", config$out_tag), ".yaml")
 )
 
 setwd("07_display_figures")
 output_dir <- normalizePath(file.path("output", config$out_tag))
 rmarkdown::render("07_display_figures.Rmd",
-  output_dir = output_dir, params = list(input_path = input_06)
+  output_dir = output_dir, params = list(input_path = input_07)
 )
 setwd("..")
 
--- validate.R
+++ validate.R
@@ -19,47 +19,49 @@ config <- yaml::read_yaml(args[[1]])
 system2("Rscript", args = c("01_prep_model_data.R", args[[1]]))
 setwd("..")
 
-# 03b -------------------------------------------------------------------------
-
-input_03b <- normalizePath(
-  paste0(file.path("01_prep_model_data/output", config$out_tag), ".yaml")
-)
-
-setwd("03b_create_census")
-system2("Rscript", args = c("create_census.R", input_03b))
-setwd("..")
-
 # 04 -------------------------------------------------------------------------
 
 input_04 <- normalizePath(
-  paste0(file.path("03b_create_census/output", config$out_tag), ".yaml")
+  paste0(file.path("03_model_select/output", config$out_tag), ".yaml")
 )
 
-setwd("04_prediction_data")
-system2("Rscript", args = c("create_prediction_data.R", input_04))
+setwd("04_create_census")
+system2("Rscript", args = c("create_census.R", input_04))
 setwd("..")
 
 # 05 -------------------------------------------------------------------------
 
 input_05 <- normalizePath(
-  paste0(file.path("04_prediction_data/output", config$out_tag), ".yaml")
+  paste0(file.path("04_create_census/output", config$out_tag), ".yaml")
 )
 
-setwd("05_figures")
-output_dir <- normalizePath(file.path("output", config$out_tag))
-rmarkdown::render("05_figures.Rmd",
-  output_dir = output_dir, params = list(input_path = input_05)
-)
+setwd("05_prediction_data")
+system2("Rscript", args = c("create_prediction_data.R", input_05))
 setwd("..")
 
-# 06 --------------------------------------------------------------------------
+# 06 -------------------------------------------------------------------------
+
 input_06 <- normalizePath(
-  paste0(file.path("05_figures/output", config$out_tag), ".yaml")
+  paste0(file.path("06_prediction_data/output", config$out_tag), ".yaml")
 )
 
-setwd("06_display_figures")
+setwd("06_create_figures")
 output_dir <- normalizePath(file.path("output", config$out_tag))
-rmarkdown::render("06_display_figures.Rmd",
+rmarkdown::render("06_create_figures.Rmd",
   output_dir = output_dir, params = list(input_path = input_06)
 )
 setwd("..")
+
+# 07 --------------------------------------------------------------------------
+input_07 <- normalizePath(
+  paste0(file.path("07_create_figures/output", config$out_tag), ".yaml")
+)
+
+setwd("07_display_figures")
+output_dir <- normalizePath(file.path("output", config$out_tag))
+rmarkdown::render("07_display_figures.Rmd",
+  output_dir = output_dir, params = list(input_path = input_07)
+)
+setwd("..")
+
+beepr::beep()
