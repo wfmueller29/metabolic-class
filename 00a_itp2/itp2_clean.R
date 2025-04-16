fb2ffbe4995337ff774b7cd3f2aea45a9e100dea
William Mueller <williammueller@williams-mcp.mynetworksettings.com>
2025-04-15 22:01:56 -0400

    Created train and test files for treatment response analysis

--- 00a_itp2/itp2_clean.R
+++ 00a_itp2/itp2_clean.R
@@ -10,3 +10,40 @@ itp_e2_16m_surv <- read.csv("data/itp_surv_17aE2_16m.csv")
 itp_e2_20m_surv <- read.csv("data/itp_surv_17aE2_20m.csv")
 itp_control_surv <- read.csv("data/itp_surv_controls.csv")
 itp_rapa_surv <- read.csv("data/itp_surv_rapa.csv")
+
+
+# create treatment variable ---------------------------------------------------
+itp_control$tx <- "control"
+itp_e2_16m$tx <- "e2_16m"
+itp_e2_20m$tx <- "e2_20m"
+itp_rapa$tx <- "rapa"
+
+# rbind data ------------------------------------------------------------------
+itp_tx_bw <- rbind(itp_control, itp_e2_16m, itp_e2_20m, itp_rapa)
+
+itp_tx_surv <- rbind(
+  itp_control_surv, itp_e2_16m_surv, itp_e2_20m_surv, itp_rapa_surv
+)
+
+itp_tx_control_surv <- rbind(
+  itp_e2_20m_surv, itp_control_surv, itp_rapa_surv, itp_e2_16m_surv
+)
+
+# only keep cohorts of interest -----------------------------------------------
+train_cohorts <- c("C2010", "C2011", "C2013")
+test_cohorts <- c("C2005", "C2016")
+
+itp_control_train <- itp_tx_bw[itp_tx_bw[, "cohort"] %in% train_cohorts, ]
+itp_control_tx_test <- itp_tx_bw[itp_tx_bw[, "cohort"] %in% test_cohorts, ]
+
+# save csvs -------------------------------------------------------------------
+
+dir.create("output", recursive = TRUE)
+train_path <- file.path("output", "itp_control_train.csv")
+write.csv(itp_control_train, train_path)
+
+test_path <- file.path("output", "itp_tx_control_test.csv")
+write.csv(itp_control_tx_test, test_path)
+
+surv_path <- file.path("output", "itp_tx_control_surv.csv")
+write.csv(itp_tx_control_surv, surv_path)
