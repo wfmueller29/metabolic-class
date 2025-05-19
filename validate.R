24e01eec300704578864e20f0d68c2c5f0cdc00a
William Mueller <williammueller@williams-mcp.mynetworksettings.com>
2025-05-18 21:51:00 -0400

    Added exit code and fixed 04 input path

--- validate.R
+++ validate.R
@@ -5,7 +5,7 @@
 args <- commandArgs(trailingOnly = TRUE)
 
 if (length(args) == 0) {
-  args[[1]] <- "x01_external_validation/input/slam_age_all.yaml"
+  args[[1]] <- "inputs/validate/slam_c1-c10_x_slam_c16-c18.yaml"
   warning("No input file provided, using: ", args[[1]])
 }
 
@@ -16,17 +16,19 @@ cat("This is our input file:", args[[1]], "\n")
 
 setwd("01_prep_model_data")
 config <- yaml::read_yaml(args[[1]])
-system2("Rscript", args = c("01_prep_model_data.R", args[[1]]))
+exit_code <- system2("Rscript", args = c("01_prep_model_data.R", args[[1]]))
+if (exit_code != 0) stop("Error was thrown from system2 command")
 setwd("..")
 
 # 04 -------------------------------------------------------------------------
 
 input_04 <- normalizePath(
-  paste0(file.path("03_model_select/output", config$out_tag), ".yaml")
+  paste0(file.path("01_prep_model_data/output", config$out_tag), ".yaml")
 )
 
 setwd("04_create_census")
-system2("Rscript", args = c("create_census.R", input_04))
+exit_code <- system2("Rscript", args = c("create_census.R", input_04))
+if (exit_code != 0) stop("Error was thrown from system2 command")
 setwd("..")
 
 # 05 -------------------------------------------------------------------------
@@ -36,7 +38,8 @@ input_05 <- normalizePath(
 )
 
 setwd("05_prediction_data")
-system2("Rscript", args = c("create_prediction_data.R", input_05))
+exit_code <- system2("Rscript", args = c("create_prediction_data.R", input_05))
+if (exit_code != 0) stop("Error was thrown from system2 command")
 setwd("..")
 
 # 06 -------------------------------------------------------------------------
