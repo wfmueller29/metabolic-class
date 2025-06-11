2c81b46c7cc5dfa48ed5499a341e28ec8f378d72
William Mueller <williammueller@williams-mcp.local>
2025-06-10 22:33:51 -0400

    Renamed sup to 97, deleted old run_8.sh

--- run_8.R
+++
@@ -1,5 +0,0 @@
-# 08 --------------------------------------------------------------------------
-
-setwd("08_sup_figures")
-rmarkdown::render("08_sup_figures.Rmd", output_format = "pdf_document")
-setwd("..")
---
+++ run_97.R
@@ -0,0 +1,5 @@
+# 08 --------------------------------------------------------------------------
+
+setwd("97_sup_figures")
+rmarkdown::render("97_sup_figures.Rmd", output_format = "pdf_document")
+setwd("..")
