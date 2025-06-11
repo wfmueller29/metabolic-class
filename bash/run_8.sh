cc32bd35add76c1e156152538abb5ce384de30f0
William Mueller <williammueller@williams-mcp.local>
2025-06-10 21:51:37 -0400

    Updated run_8.R and bash/run_8.sh

---
+++ bash/run_8.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+Rscript run_8.R inputs/train/slam_c1-c10_age_all_bwfatgluc.yaml ; \
+Rscript run_8.R inputs/train/slam_c1-c10_age_fb6_bwfatgluc.yaml ; \
+Rscript run_8.R inputs/train/slam_c1-c10_age_fhet3_bwfatgluc.yaml ; \
+Rscript run_8.R inputs/train/slam_c1-c10_age_mb6_bwfatgluc.yaml ; \
+Rscript run_8.R inputs/train/slam_c1-c10_age_mhet3_bwfatgluc.yaml ; \
+Rscript run_8.R inputs/train/itp_c10c11c13c16_age_controls_bw.yaml
--- run_8.R
+++ run_8.R
@@ -1,26 +1,5 @@
-# This will run just the 08_display_figures
-
-args <- commandArgs(trailingOnly = TRUE)
-
-if (length(args) == 0) {
-  args[[1]] <- "01_prep_model_data/input/test_local.yaml"
-  warning("No input file provided, using: ", args[[1]])
-}
-
-args[[1]] <- normalizePath(args[[1]])
-cat("This is our input file:", args[[1]], "\n")
-config <- yaml::read_yaml(args[[1]])
-
 # 08 --------------------------------------------------------------------------
-input_08 <- normalizePath(
-  paste0(file.path("06_create_figures/output", config$out_tag), ".yaml")
-)
 
 setwd("08_sup_figures")
-output_dir <- normalizePath(file.path("output", config$out_tag))
-rmarkdown::render("08_sup_figures.Rmd",
-  output_dir = output_dir, params = list(input_path = input_08),
-  output_format = "word_document"
-)
+rmarkdown::render("08_sup_figures.Rmd", output_format = "pdf_document")
 setwd("..")
-
