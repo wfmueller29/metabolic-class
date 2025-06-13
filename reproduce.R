f480bba7ae77bbe5a4b9c552822a4102890f5623
William Mueller <williammueller@williams-mcp.local>
2025-06-13 02:04:51 -0400

    Created reproduce.R file

---
+++ reproduce.R
@@ -0,0 +1,52 @@
+# Reproduce entire analysis
+
+# Preprocess  -----------------------------------------------------------------
+ecode <- system2("Rscript", args = c("preprocess.R"))
+if (ecode != 0) stop("Error was thrown from system2 command")
+
+# Train  ----------------------------------------------------------------------
+yaml_files <- c(
+  "inputs/train/slam_c1-c10_age_all_bwfatgluc.yaml",
+  "inputs/train/slam_c1-c10_age_fb6_bwfatgluc.yaml",
+  "inputs/train/slam_c1-c10_age_fhet3_bwfatgluc.yaml",
+  "inputs/train/slam_c1-c10_age_mb6_bwfatgluc.yaml",
+  "inputs/train/slam_c1-c10_age_mhet3_bwfatgluc.yaml",
+  "inputs/train/slam_c1-c10_age_het3_bwfatgluc.yaml",
+  "inputs/train/itp_c10c11c13c16_age_controls_bw.yaml"
+)
+
+for (yaml in yaml_files) {
+  cat("Running:", yaml, "\n")
+  ecode <- system2("Rscript", args = c("train.R", yaml))
+  if (ecode != 0) stop(paste("Error in:", yaml))
+}
+
+# Validate  -------------------------------------------------------------------
+yaml_validate <- "inputs/validate/slam_c1-c10_x_slam_c16-c18.yaml"
+ecode <- system2("Rscript", args = c("validate.R", yaml_validate))
+if (ecode != 0) stop("Error was thrown from system2 command")
+
+# Predict ---------------------------------------------------------------------
+yaml_files <- c(
+  "inputs/predict/slam_c1-c10_p_slam_c16-c18.yaml",
+  "inputs/predict/itp_controls_p_treatment.yaml"
+)
+
+for (yaml in yaml_files) {
+  cat("Running:", yaml, "\n")
+  ecode <- system2("Rscript", args = c("predict.R", yaml))
+  if (ecode != 0) stop(paste("Error in:", yaml))
+}
+
+# Treatment Response ----------------------------------------------------------
+rmarkdown::render("99_treatment_response/treatment_response.Rmd")
+
+# Figures ---------------------------------------------------------------------
+rmarkdown::render("figures/primary_figures.Rmd",
+  output_format = "pdf_document",
+  output_file = "Primary Figures.pdf"
+)
+rmarkdown::render("figures/sup_figures.Rmd",
+  output_format = "pdf_document",
+  output_file = "Supplemental Material.pdf"
+)
