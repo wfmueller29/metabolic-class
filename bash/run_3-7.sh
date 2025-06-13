8b6950c5364cf8ebb2da857cdec0ab300245096c
William Mueller <williammueller@williams-mcp.local>
2025-06-13 00:09:29 -0400

    Added lmerTest and fixed run_3-7.sh for validation

--- bash/run_3-7.sh
+++ bash/run_3-7.sh
@@ -6,5 +6,5 @@ Rscript run_3-7.R inputs/train/slam_c1-c10_age_fhet3_bwfatgluc.yaml ; \
 Rscript run_3-7.R inputs/train/slam_c1-c10_age_mb6_bwfatgluc.yaml ; \
 Rscript run_3-7.R inputs/train/slam_c1-c10_age_mhet3_bwfatgluc.yaml ; \
 Rscript run_3-7.R inputs/train/itp_c10c11c13c16_age_controls_bw.yaml ; \
-Rscript run_3-7.R inputs/validate/slam_c1-c10_x_slam_c16-c18.yaml
+Rscript validate.R inputs/validate/slam_c1-c10_x_slam_c16-c18.yaml
 
