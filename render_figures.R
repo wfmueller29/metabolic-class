b1322b8bc340ab9ebab67337027c1538fbb48fb5
William Mueller <williammueller@williams-mcp.mynetworksettings.com>
2025-06-29 19:24:13 -0400

    Added run pub_ready figs to render_figures

--- render_figures.R
+++ render_figures.R
@@ -1,4 +1,10 @@
 # Figures ---------------------------------------------------------------------
+
+setwd("96_pub_ready_figs/")
+ecode <- system2("Rscript", "pub_ready_figs.R")
+if (ecode != 0) stop(paste("Error in pub_ready_figs.R"))
+setwd("..")
+
 rmarkdown::render("figures/primary_figures.Rmd",
   output_format = "pdf_document",
   output_dir = "figures/output",
