6f7d243ce29ed733252e80a38b143e52628f0446
William Mueller <williammueller@williams-mcp.mynetworksettings.com>
2025-09-28 23:53:22 -0400

    Created itp_geno_surv

--- 00a_clean_itp_geno/00a_clea_itp_geno.R
+++ 00a_clean_itp_geno/00a_clea_itp_geno.R
@@ -36,6 +36,8 @@ itp_geno_f <- itp_geno[itp_geno$sex_M == 0, ]
 
 itp_geno_treat <- readxl::read_xlsx(path = "data/Mortality_Class_Predictions_RW_JP_fixed(84).xlsx", sheet = 6)
 itp_geno_treat_census <- readxl::read_xlsx(path = "data/Mortality_Class_Predictions_RW_JP_fixed(84).xlsx", sheet = 5)
+itp_geno_treat_surv <- consoler::rename(itp_geno_treat_census, c(le_wk = "lifespan_wk", tx = "group (tx)"))
+itp_geno_treat_surv <- itp_geno_treat_surv[, c("id", "idno", "cohort", "site", "strain", "sex", "tx", "le_wk")]
 itp_geno_treat <- consoler::rename(itp_geno_treat, c(age_wk = "age_of_wegith_wk"))
 itp_geno_treat_census <- itp_geno_treat_census[c("id", "idno")]
 itp_geno_treat <- merge(itp_geno_treat, itp_geno_treat_census, by = "idno")
@@ -67,3 +69,4 @@ write.csv(itp_geno_f, "output/itp_genotyped_bw_og_f.csv")
 write.csv(itp_geno_treat, "output/itp_genotyped_treat_bw_og.csv")
 write.csv(itp_geno_treat_m, "output/itp_genotyped_treat_bw_og_m.csv")
 write.csv(itp_geno_treat_f, "output/itp_genotyped_treat_bw_og_f.csv")
+write.csv(itp_geno_treat_surv, "output/itp_genotyped_treat_surv.csv")
