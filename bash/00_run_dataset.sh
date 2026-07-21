bec7062fa345c029fc2708b7c2b24c4df2aed1d2
Josh Preston <jdpreston30@gmail.com>
2026-07-21 10:39:49 -0400

    removed bash/ and associated .sh scripts as these were associated with Biowulf development

--- bash/00_run_dataset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-module load R/4.1 || echo WARNING: Could not load R module, continuing anyway
-
-cd 01_dataset/
-
-Rscript render_dataset.R
--- bash/run_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-Rscript train.R inputs/train/slam_c1-c10_age_all_bwfatgluc.yaml ; \
-Rscript train.R inputs/train/slam_c1-c10_age_fb6_bwfatgluc.yaml ; \
-Rscript train.R inputs/train/slam_c1-c10_age_fhet3_bwfatgluc.yaml ; \
-Rscript train.R inputs/train/slam_c1-c10_age_mb6_bwfatgluc.yaml ; \
-Rscript train.R inputs/train/slam_c1-c10_age_mhet3_bwfatgluc.yaml ; \
-Rscript train.R inputs/train/itp_c10c11c13c16_age_controls_bw.yaml ; \
-Rscript validate.R inputs/validate/slam_c1-c10_x_slam_c16-c18.yaml
